4991a9096ed03d97aaac46058218726aeb733900
Zied ECHEIKH <ziedecheikh@Zieds-MBP.lan>
2024-05-13 08:38:54 +0200

    design html pages

--- src/main/java/tu/startup/tokenator/HomeController.java
+++ src/main/java/tu/startup/tokenator/HomeController.java
@@ -20,46 +20,71 @@ public class HomeController {
 
     private String clientId;
 
+    private boolean isAuthenticated;
+
+    private String accessToken;
+
+    private String refreshToken;
+
+    private String scopes;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("isAuthenticated", this.isAuthenticated);
         return "redirect:http://localhost:8383/index";
     }
 
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("isAuthenticated", this.isAuthenticated);
         return "index";
     }
 
     @GetMapping("/oidc")
-    public String oidcClient() {
+    public String oidcClient(Model model) {
+        model.addAttribute("isAuthenticated", this.isAuthenticated);
         this.clientId = "oidc-client";
-        return "oidc";
+        if (isAuthenticated) {
+            model.addAttribute("accessToken", this.accessToken);
+            model.addAttribute("refreshToken", this.refreshToken);
+            model.addAttribute("scopes", this.scopes);
+            return "oidc";
+        }
+        return "index";
     }
 
     @GetMapping("/authorized/{clientId}")
     public String accessToken(Model model, @RequestParam String code, @PathVariable String clientId) {
+        this.clientId = "oidc-client";
         if (!this.clientId.equals(clientId)) {
             model.addAttribute("clientId", this.clientId);
             return "authorized";
         }
+        if (!this.isAuthenticated) {
+            MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+            parameters.add("grant_type", "authorization_code");
+            parameters.add("code", code);
+            parameters.add("redirect_uri", "http://localhost:8383/authorized/" + clientId);
 
-        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
-        parameters.add("grant_type", "authorization_code");
-        parameters.add("code", code);
-        parameters.add("redirect_uri", "http://localhost:8383/authorized/" + clientId);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+            headers.setBasicAuth("oidc-client", "secretto");
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.setBasicAuth("oidc-client", "secretto");
-
-        String baseUrl = "http://localhost:8080";
-        RequestEntity<MultiValueMap<String, String>> requestEntity = new RequestEntity<>(parameters, headers, HttpMethod.POST, URI.create(baseUrl + "/oauth2/token"));
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Map<String, String>> responseEntity = restTemplate.exchange(requestEntity, new ParameterizedTypeReference<Map<String, String>>() {
-        });
-        model.addAttribute("accessToken", responseEntity.getBody().get("access_token"));
-
-        return "authorized";
+            String baseUrl = "http://localhost:8080";
+            RequestEntity<MultiValueMap<String, String>> requestEntity = new RequestEntity<>(parameters, headers, HttpMethod.POST, URI.create(baseUrl + "/oauth2/token"));
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<Map<String, String>> responseEntity = restTemplate.exchange(requestEntity, new ParameterizedTypeReference<Map<String, String>>() {
+            });
+            this.isAuthenticated = true;
+            this.accessToken = responseEntity.getBody().get("access_token");
+            this.refreshToken = responseEntity.getBody().get("refresh_token");
+            this.scopes = responseEntity.getBody().get("scope");
+        }
+        model.addAttribute("isAuthenticated", this.isAuthenticated);
+        model.addAttribute("accessToken", this.accessToken);
+        model.addAttribute("refreshToken", this.refreshToken);
+        model.addAttribute("scopes", this.scopes);
+        return "oidc";
     }
 }
